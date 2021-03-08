5357ef7b064668a93bd4a3a485dc003bc77dd263
Alexander <uo266499@uniovi.es>
2021-03-08 18:53:32 +0100

    Cambios hechos al Map.js para funcionar correctamente

--- webapplication/web-application/src/Map.js
+++ webapplication/web-application/src/Map.js
@@ -1,58 +1,16 @@
-import React, { Component } from 'React'
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component } from 'react'
+import { Map, GoogleApiWrapper,Marker} from 'google-maps-react';
 
 export class MapComponent extends Component {
-
-    render() {
-
-        const markers = retrieveMarkers();
-
-        return (<div className="map-area">
-
-                <Map
-
-                    google={this.props.google}
-
-                    zoom={14}
-
-                    center={{
-
-                        lat: 43.25018525761058,
-
-                        lng: -5.782307057287958
-
-                    }}>
-                    
-                    {markers.map((marker)=>{
-                        return <Marker key={marker.name}
-
-                        icon={{
-                    
-                            url: 'https://www.flaticon.es/svg/vstatic/svg/4251/4251474.svg?token=exp=1614709424~hmac=4681d68d572f32c965509523f8cd338f',
-                    
-                            anchor: new google.maps.Point(17, 46),
-                    
-                            scaledSize: new google.maps.Size(37, 37)
-                    
-                        }}
-                    
-                        position={{
-                    
-                            lat: marker.lat,
-                    
-                            lng: marker.lng
-                    
-                        }}
-                    
-                        onClick={() => this.openinfo(marker)}
-                        />
-                     })}
-
-                </Map>
-
-            </div>);
-
-    }
+        constructor(){
+            super();
+            navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+        }
+    
+        getPosicion(posicion){
+            this.long= posicion.coords.longitude; 
+            this.lat= posicion.coords.latitude;  
+        }
 
     openinfo(marker) {
         //display the marker information
@@ -84,11 +42,72 @@ export class MapComponent extends Component {
             }
         ];
     }
+
+    render() {
+        var markers = this.retrieveMarkers();
+
+        
+        return (<div className="map-area">
+
+                <Map
+                    center={{
+
+                        lat: this.lat,
+
+                        lng: this.long
+
+                    }}
+                    centerAroundCurrentLocation={true}
+                    
+                    google={this.props.google}
+                    zoom={11}
+                    
+                    >
+                    <Marker 
+                        position={{
+                    
+                            lat: 43.364739393028024,
+                            lng: -5.8507182415799575
+                    
+                        }}
+                        title={"La capital de Asturias!"}
+                        >
+                    </Marker>
+
+                    <Marker 
+                        position={{
+                    
+                            lat: 43.545142258113735,
+                            lng: -5.662559315448055
+                    
+                        }}
+                        title={"Una ciudad preciosa!"}
+                        >
+                    </Marker>
+                    
+                    <Marker 
+                        position={{
+                    
+                            lat: 43.56040003876269,
+                            lng: -5.924200713062158
+                    
+                        }}
+                        title={"Una zona muy industrial!"}
+                        >
+                    </Marker>
+                    
+                </Map>
+
+            </div>);
+            
+    }
 }
 
 export default GoogleApiWrapper({
 
-    apiKey: ("AIzaSyBSuJOh_jsU3BGuOip776L9RW-XZGU8Dsk")
+    apiKey: ("AIzaSyC6j4mF6blrc4kZ54S6vYZ2_FpMY9VzyRU")
+    
+    //AIzaSyBSuJOh_jsU3BGuOip776L9RW-XZGU8Dsk
 
 })(MapComponent);
  
