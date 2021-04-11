63f66baa76add0c30f6531e79b343b9ac4e91304
uo271913 <uo271913@uniovi.es>
2021-04-11 17:55:32 +0200

    updating api.js and users.js from restapi

--- restapi/api.js
+++ restapi/api.js
@@ -2,26 +2,48 @@ const express = require("express")
 const User = require("./models/users")
 const router = express.Router()
 
-// Get all users
-router.get("/users/list", async (req, res) => {
-    const users = await User.find({}).sort('-_id') //Inverse order
+// Find friends that are near the user (location) in session
+router.post("/users/list", async (req, res) => {
+    let location = req.body.location; // [longitude, latitude]
+    let friends = req.body.friends; // [webId1, webId2, ...]
+
+    const users = await User.find({
+        webId: { 
+            $in: friends 
+        },
+        location: { 
+            $near: {
+                $geometry: {
+                    type: "Point",
+                    coordinates: location
+                },
+                $maxDistance: 100, // Distance in meters
+                $minDistance: 0
+            } 
+        }
+    })
 	res.send(users)
 })
 
-//register a new user
+// Add user (webId) or update its location if it already exists
 router.post("/users/add", async (req, res) => {
-    let name = req.body.name;
-    let email = req.body.email;
-    //Check if the device is already in the db
-    let user = await User.findOne({ email: email })
-    if (user)
-        res.send({error:"Error: This user is already registered"})
-    else{
+    let webId = req.body.webId;
+    let location = req.body.location;
+    //Check if the user is already in the db
+    let user = await User.findOne({ webId: webId })
+    if (user) {
+        user.location = location;
+        await user.save();
+        res.send(user)
+    } else {
         user = new User({
-            name: name,
-            email: email,
-        })
-        await user.save()
+            webId: webId,
+            location: {
+                type: "Point",
+                coordinates: location
+            }
+        });
+        await user.save();
         res.send(user)
     }
 })
--- restapi/models/users.js
+++ restapi/models/users.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose")
 
-
 const schema = mongoose.Schema({
-    name: String,
-    email: String
+    webId: String, // Unique identifier (Solid)
+    location: { // Point (GeoJSON)
+        type: { 
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: { // Longitude first, not latitude
+            type: [Number],
+            required: true
+        }
+    }
 })
+
+schema.index({ location: "2dsphere" }); // Index creation over location
+                                        // to speed up geospatial queries
 
 module.exports = mongoose.model("User", schema)
