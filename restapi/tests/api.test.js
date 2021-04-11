b929fe5486b241f3b21d8044a7e0bb24b12bdd14
uo271913 <uo271913@uniovi.es>
2021-04-11 21:06:31 +0200

    restapi creation test

--- restapi/tests/api.test.js
+++ restapi/tests/api.test.js
@@ -27,22 +27,34 @@ afterAll(async () => {
  */
 describe('user ', () => {
     /**
+     * Tests that a user can be created without any error.
+     */
+    it('can be created correctly', async () => {
+        webId = 'webId'
+        location = [-5.66152, 43.53573]
+        const response = await request(app).post('/api/users/add').send({webId: webId,location: location}).set('Accept', 'application/json')
+        expect(response.statusCode).toBe(200);
+        expect(response.body.webId).toBe(webId);
+        expect(response.body.location.coordinates).toStrictEqual(location);
+    });
+
+    /**
      * Test that we can list users without any error.
      */
-    it('can be listed',async () => {
+    /*it('can be listed',async () => {
         const response = await request(app).get("/api/users/list");
         expect(response.statusCode).toBe(200);
-    });
+    });*/
 
     /**
      * Tests that a user can be created through the productService without throwing any errors.
      */
-    it('can be created correctly', async () => {
+    /*it('can be created correctly', async () => {
         username = 'Pablo'
         email = 'pablo@uniovi.es'
         const response = await request(app).post('/api/users/add').send({name: username,email: email}).set('Accept', 'application/json')
         expect(response.statusCode).toBe(200);
         expect(response.body.name).toBe(username);
         expect(response.body.email).toBe(email);
-    });
+    });*/
 });
