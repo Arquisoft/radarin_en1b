5b44c38c55c6a43ef4cb178f588f187a9018e3f5
uo271913 <uo271913@uniovi.es>
2021-04-13 01:20:19 +0200

    adding mobileapp and restapi

---
+++ mobileapplication/mobile-application/src/components/MainView.js
@@ -0,0 +1,22 @@
+
+import MapComponent from './Map'
+//import { FOAF } from "@inrupt/lit-generated-vocab-common";
+import { useSession } from "@inrupt/solid-ui-react";
+
+
+const MainView = () => {
+
+   const { session } = useSession();
+   // const name =   await data[session.info.webId].name;
+
+    return (
+        <div className="App">
+            <h1>Radarin map preliminary version</h1>
+           
+        <MapComponent />
+        </div>
+    );
+}
+
+
+export default MainView;
---
+++ restapi/models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+    webId: String, // Unique identifier (Solid)
+    location: { // Point (GeoJSON)
+        type: { 
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: { // Longitude first, not latitude
+            type: [Number],
+            required: true
+        }
+    },
+    lastUpdate: Date
+})
+//,{ timestamps: true })
+
+schema.index({ location: "2dsphere" }) // Index creation over location
+                                       // to speed up geospatial queries
+
+module.exports = mongoose.model("User", schema)
