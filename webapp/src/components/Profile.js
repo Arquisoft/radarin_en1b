c0bd8bcecb40c324dfc60c776d006f865a05a978
Alexander <uo266499@uniovi.es>
2021-04-26 17:08:10 +0200

    change to Profile for now

--- webapp/src/components/Profile.js
+++ webapp/src/components/Profile.js
@@ -3,7 +3,7 @@ import { Figure } from 'react-bootstrap';
 import getPhoto from '../utils/solidAccessing/ObtainImage';
 import userPhoto from '../static/user.svg';
 import '../css/Navbar.css'
-export default async function Profile ({session}) {
-    
+export default  function Profile () {
+    return null;
 }
 
