e479d1a4563d95f10f4b36b2e68bde8eed515d25
Alexander <uo266499@uniovi.es>
2021-05-04 06:16:23 +0200

    declaration issue

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -4,7 +4,7 @@ const feature = loadFeature("./features/register-form.feature");
 defineFeature(feature, (test) => {
   
   beforeEach(async () => {
-    await global.page.goto("http://localhost:3000")
+    await global.page.goto("http://localhost:3000", {timeout: 60000});
   })
 
   test("The user tries to access views where login is needed: Map", ({given,when,then}) => {
@@ -18,7 +18,7 @@ defineFeature(feature, (test) => {
     });
 
     when("The user tries to access the map", async () => {
-      await global.page.goto("http://localhost:3000/map")
+      await global.page.goto("http://localhost:3000/map", {timeout: 60000});
     });
 
     then("A message should be shown in the screen", async () => {
@@ -37,8 +37,8 @@ defineFeature(feature, (test) => {
       username = "none"
     });
 
-    when("I try to access manage friends", async () => {
-      await global.page.goto("http://localhost:3000/friends")
+    when("The user tries to access manage friends", async () => {
+      await global.page.goto("http://localhost:3000/friends", {timeout: 60000})
     });
 
     then("A message should be shown in the screen", async () => {
