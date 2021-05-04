31b1f5539b060f1e65d2e4844cd6f373a9143e3f
Alexander <uo266499@uniovi.es>
2021-05-04 05:59:08 +0200

    trying e2e

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -3,10 +3,11 @@ const feature = loadFeature("./features/register-form.feature");
 
 defineFeature(feature, (test) => {
   
-  /*beforeEach(async () => {
+  beforeEach(async () => {
     await global.page.goto("http://localhost:3000")
   })
 
+  /*
   test("The user is not registered in the site", ({given,when,then}) => {
     
     let email;
@@ -29,8 +30,49 @@ defineFeature(feature, (test) => {
 
     then("A welcome message should be shown in the screen", async () => {
     });
+  });*/
+
+  test("The user tries to access views where login is needed: Map", ({given,when,then}) => {
+    
+    let email;
+    let username;
+
+    given("An unregistered user", () => {
+      email = "none@none.com"
+      username = "none"
+    });
+
+    when("I try to access the map", async () => {
+      await global.page.goto("http://localhost:3000/map")
+    });
+
+    then("A message should be shown in the screen", async () => {
+      await expect(page).toMatch("You are not logged in!")
+      await expect(page).toMatch("To be able to view this you need to be logged in")
+    });
   });
 
+  test("The user tries to access views where login is needed: Manage Friends", ({given,when,then}) => {
+    
+    let email;
+    let username;
+
+    given("An unregistered user", () => {
+      email = "none@none.com"
+      username = "none"
+    });
+
+    when("I try to access manage friends", async () => {
+      await global.page.goto("http://localhost:3000/friends")
+    });
+
+    then("A message should be shown in the screen", async () => {
+      await expect(page).toMatch("You are not logged in!")
+      await expect(page).toMatch("To be able to view this you need to be logged in")
+    });
+  });
+ 
+  /*
   test("The user is already registered in the site", ({ given, when, then }) => {
     
     given("An already registered user", () => {
